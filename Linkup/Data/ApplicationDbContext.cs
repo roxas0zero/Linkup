5e9b21e2cc727cf8f86e099ef19498c0a88c1351
Abdullah Hashim <hasabdullah@saudia.com>
2020-10-21 01:11:02 +0300

    added db context

---
+++ Linkup/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Linkup.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Linkup.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<Skill> Skills { get; set; }        
+    }
+}
