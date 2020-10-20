de4a1c4eebdca7435bea371312dc7c867855dbd1
Abdullah Hashim <hasabdullah@saudia.com>
2020-10-20 21:35:45 +0300

    added models

---
+++ Linkup/Models/ApplicationUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linkup.Models
+{
+    public class ApplicationUser
+    {
+        public string CorpId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+    }
+}
