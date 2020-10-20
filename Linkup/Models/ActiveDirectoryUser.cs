c27d31f8fc589e9b3d4681de9323d64c0158e776
Abdullah Hashim <hasabdullah@saudia.com>
2020-10-21 01:11:51 +0300

    added active directory service

---
+++ Linkup/Models/ActiveDirectoryUser.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linkup.Models
+{
+    public class ActiveDirectoryUser
+    {
+        public string CorpId { get; set; }
+        public string PRN { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }        
+    }
+}
---
+++ Linkup/Services/Interfaces/IActiveDirectoryService.cs
@@ -0,0 +1,14 @@
+﻿using Linkup.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linkup.Services.Interfaces
+{
+    public interface IActiveDirectoryService
+    {
+        ActiveDirectoryUser GetUserInfo(string corpId);
+        string GetUserFirstName(string corpId);
+    }
+}
