8a871a4faabe067cc72499cfda2b36eeee6203da
Abdullah Hashim <hasabdullah@saudia.com>
2020-10-26 00:38:44 +0300

    implemented project creation page

---
+++ Linkup/Controllers/ProjectController.cs
@@ -0,0 +1,55 @@
+﻿using System.Threading.Tasks;
+using Linkup.Models;
+using Linkup.Services.Interfaces;
+using Linkup.ViewModels.Project;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Linkup.Controllers
+{
+    public class ProjectController : Controller
+    {
+        private readonly IProjectService projectService;
+
+        public ProjectController(IProjectService projectService)
+        {
+            this.projectService = projectService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(ProjectCreateViewModel projectViewModel)
+        {
+            var project = new Project
+            {
+                Title = projectViewModel.Title,
+                Description = projectViewModel.Description,
+                DueDate = projectViewModel.DueDate
+            };
+            await projectService.Create(project);
+            return RedirectToAction(nameof(AddDetails), new { Id = project.Id });
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> AddDetails(int id)
+        {
+            var project = await projectService.GetById(id);
+            var model = new ProjectCreateViewModel
+            {
+                Title = project.Title,
+                Description = project.Description,
+                DueDate = project.DueDate
+            };
+            return View(model);
+        }
+    }
+}
---
+++ Linkup/ViewModels/Project/ProjectCreateViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linkup.ViewModels.Project
+{
+    public class ProjectCreateViewModel
+    {        
+        public string Title { get; set; }
+        public string Description { get; set; }
+        [Display(Name ="Due Date")]
+        public DateTime DueDate { get; set; }        
+        public List<SkillViewModel> NeededSkills { get; set; }
+        public List<InterestViewModel> RelatedInterests { get; set; }               
+    }
+
+    public class SkillViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class InterestViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
